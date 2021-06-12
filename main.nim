323334e6e49d459ed739e16d1d438e0aa25c2005
Pavel Levshic <pavellevshic@gmail.com>
2021-06-13 02:22:04 +0300

    (IEconService) GetTradeHistory
    GetTradeHistory

--- main.nim
+++ main.nim
@@ -1,6 +1,11 @@
 import json, httpclient, strutils
 # for compile use -d:ssl
 
+# STEAM Web API Key
+var steamWebAPIKey = readFile("SteamApiKey.txt")
+
+
+
 var client = newHttpClient()
 
 # Decode specific chars to % in URL
@@ -16,18 +21,66 @@ proc escapeLink(s: string): string =
 
 
 type
-    MinItem = object
-        success: bool
-        lowest_price: string
-        volume: string
-        median_price: string
+  MinItem = object
+    success: bool
+    lowest_price: string
+    volume: string
+    median_price: string
+
 
 proc getMinItem(appid: int, vallet: int, market_hash_name: string): MinItem =
-    let url = "https://steamcommunity.com/market/priceoverview/?appid="&intToStr(appid)&"&currency="&intToStr(vallet)&"&market_hash_name="&escapeLink(market_hash_name)
-    let jsonObject = parseJson(client.getContent(url))
-    return to(jsonObject, MinItem)
+  let url = "https://steamcommunity.com/market/priceoverview/?appid="&intToStr(
+      appid)&"&currency="&intToStr(vallet)&"&market_hash_name="&escapeLink(market_hash_name)
+  let jsonObject = parseJson(client.getContent(url))
+  return to(jsonObject, MinItem)
 
 
-let test = getMinItem(730,1,"StatTrak™ M4A1-S | Hyper Beast (Minimal Wear)")  
 
-echo test
+echo getMinItem(730, 1, "★ Karambit | Doppler (Factory New)")
+
+
+
+######################################
+# Trade Market Service (IEconService)
+# https://partner.steamgames.com/doc/webapi/IEconService
+######################################
+
+
+type
+  TradeAsset = object
+    appid: int
+    contextid: string
+    assetid: string
+    amount: string
+    classid: string
+    instanceid: string
+    new_assetid: string
+    new_contextid: string
+
+  Trade = object
+    tradeid: string
+    steamid_other: string
+    time_init: int
+    status: int
+    assets_received*: seq[TradeAsset]
+    assets_given*: seq[TradeAsset]
+
+
+
+proc tradeHistory(max_trades: int): seq[Trade] =
+  let url = "https://api.steampowered.com/IEconService/GetTradeHistory/v1/?key="&steamWebAPIKey&"&max_trades="&intToStr(max_trades)
+  let jsonObject = parseJson(client.getContent(url))
+  let jsonResponse = jsonObject["response"]
+  let jsonTrades = jsonResponse["trades"]
+  doAssert jsonTrades.kind == JArray
+  for jsonTrade in jsonTrades:
+    if not jsonTrade.hasKey("assets_received"):
+      jsonTrade.add("assets_received", parseJson("[]"))
+    if not jsonTrade.hasKey("assets_given"):
+      jsonTrade.add("assets_given", parseJson("[]"))
+    result.add(to(jsonTrade, Trade))
+  return result
+
+
+echo tradeHistory(1)
+
